dcf1b00399c98a44c0962be62cbfacf03b9be9cd
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-08-09 16:40:52 +0200

    OPSEXP-1605 Add workflow to build Imagemagick RPM for RockyLinux 8 (#1)

---
+++ .github/actions/rockylinux-build/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+if [ -z "$GITHUB_WORKSPACE" ]; then
+    echo "GITHUB_WORKSPACE is not set"
+    exit 1
+fi
+
+DEST_DIR=$GITHUB_WORKSPACE/rpms
+
+echo "Copying prebuilt RPMs to $DEST_DIR"
+mkdir -p "$DEST_DIR"
+cp /root/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
+
+exit 0
