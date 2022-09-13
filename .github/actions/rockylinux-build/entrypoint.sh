0390fcab11be12ae6cc12802bb7dffbac1d89d8e
slohe1 <saurabh.lohe@globallogic.com>
2022-09-13 19:11:53 +0530

    Building imagemagick 7.1.0-16

--- .github/actions/rockylinux-build/entrypoint.sh
+++ .github/actions/rockylinux-build/entrypoint.sh
@@ -5,11 +5,18 @@ if [ -z "$GITHUB_WORKSPACE" ]; then
     exit 1
 fi
 
-DEST_DIR=$GITHUB_WORKSPACE/rpms
+OS=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
+
+DEST_DIR=$GITHUB_WORKSPACE/packages
 
 echo "Copying prebuilt RPMs for $IMAGEMAGICK_VERSION to $DEST_DIR"
 mkdir -p "$DEST_DIR"
-cp /root/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
+
+if [ "$OS" = "Ubuntu" ]; then \
+   cp /root/*.deb "$DEST_DIR" 
+else 
+   cp /root/rpmbuild/RPMS/x86_64/*.rpm  "$DEST_DIR"
+fi
 
 echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
 
