58123df6fcb02150c01871e689d334cec0b41d41
slohe1 <saurabh.lohe@globallogic.com>
2022-09-16 16:25:04 +0530

    OPSEXP-1700 fixup Centos

---
+++ .github/actions/centos-build/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+if [ -z "$GITHUB_WORKSPACE" ]; then
+    echo "GITHUB_WORKSPACE is not set"
+    exit 1
+fi
+
+DEST_DIR=$GITHUB_WORKSPACE/rpms
+
+echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
+mkdir -p "$DEST_DIR"
+
+if [ -e /etc/debian_version ]; then
+   cp /root/*.deb "$DEST_DIR" 
+else
+   if [ -e /etc/redhat-release ]; then
+      cp /root/rpmbuild/RPMS/x86_64/*.rpm  "$DEST_DIR"
+   fi
+ fi
+
+echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
+
+exit 0
