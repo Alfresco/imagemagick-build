74f71aa3c7ed0a56910df362f3b5a7fcbe2c5dad
slohe1 <105858985+slohe1@users.noreply.github.com>
2022-09-23 13:34:00 +0530

    OPSEXP-1700 Imagemagick-7.1.0-16 build  (#3)
    Co-authored-by: Giovanni Toraldo <giovanni.toraldo@hyland.com>

--- .github/actions/Ubuntu-build/entrypoint.sh
+++ .github/actions/Ubuntu-build/entrypoint.sh
@@ -5,11 +5,11 @@ if [ -z "$GITHUB_WORKSPACE" ]; then
     exit 1
 fi
 
-DEST_DIR=$GITHUB_WORKSPACE/rpms
+DEST_DIR=$GITHUB_WORKSPACE/packages
 
 echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
 mkdir -p "$DEST_DIR"
-cp /root/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
+cp /root/*.deb "$DEST_DIR" 
 
 echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
 
--- .github/actions/centos-build/entrypoint.sh
+++ .github/actions/centos-build/entrypoint.sh
@@ -9,7 +9,7 @@ DEST_DIR=$GITHUB_WORKSPACE/rpms
 
 echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
 mkdir -p "$DEST_DIR"
-cp /root/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
+cp /root/rpmbuild/RPMS/x86_64/*.rpm  "$DEST_DIR"
 
 echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
 
