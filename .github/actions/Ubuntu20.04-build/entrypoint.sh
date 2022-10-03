0ea7c61b6a67dc35cced07c9aff8cb07ddf1029d
slohe1 <saurabh.lohe@globallogic.com>
2022-10-03 17:26:47 +0530

    OPSEXP-1739 Imagemagick build changes

--- .github/actions/Ubuntu20.04-build/entrypoint.sh
+++ .github/actions/Ubuntu20.04-build/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+apt-get update && apt-get upgrade -y &&  apt-get install build-essential git automake equivs -y
+
+git clone --depth 1 -b $1 https://github.com/ImageMagick/ImageMagick.git
+
+cd ImageMagick && \
+    mv /tmp/debian . && \
+    apt-get install -y dpkg-dev devscripts gawk ghostscript gsfonts libzstd-dev libbz2-dev libdjvulibre-dev libfftw3-dev fontconfig libfreetype6-dev libgs-dev libjbig-dev libjpeg8-dev libjpeg-turbo8-dev liblcms2-dev liblqr-1-0-dev libltdl-dev liblzma-dev libopenexr-dev libopenjp2-7-dev libpango1.0-dev libperl-dev libpng-dev libraqm-dev libraw-dev librsvg2-dev libtiff5-dev libwebp-dev libwmf-dev libx11-dev libxext-dev libxml2-dev libxt-dev libzip-dev zlib1g-dev && \
+    yes | mk-build-deps -Bi && dpkg-buildpackage -b -uc
+
+
 if [ -z "$GITHUB_WORKSPACE" ]; then
     echo "GITHUB_WORKSPACE is not set"
     exit 1
@@ -7,10 +17,10 @@ fi
 
 DEST_DIR=$GITHUB_WORKSPACE/packages
 
-echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
+echo "Copying prebuilt packages for $1 to $DEST_DIR"
 mkdir -p "$DEST_DIR"
 cp /root/*.deb "$DEST_DIR" 
 
-echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
+echo "::set-output name=built-version::$1"
 
 exit 0
--- .github/actions/rockylinux-build/entrypoint.sh
+++ .github/actions/rockylinux-build/entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/bash -e
 
+# Build dependencies
+dnf install -y epel-release git make wget 'dnf-command(config-manager)' && \
+    dnf config-manager --set-enabled powertools && \
+    dnf install -y rpm-build yum-utils && \
+    dnf group install -y "Development Tools" && \
+    dnf clean all
+
+# Imagemagick sources
+git clone --depth 1 -b $1 https://github.com/ImageMagick/ImageMagick.git
+
+# Generate updated .src.rpm
+cd ImageMagick && \
+    ./configure && \
+    make dist-xz && \
+    sed -i '/BuildRequires.*lqr/d' ImageMagick.spec && \
+    make srpm
+
+# Finally build rpm
+ cd /github/workspace/ImageMagick && \
+    yum-builddep -y ImageMagick-$1.src.rpm && \
+    rpmbuild --rebuild --nocheck ImageMagick-$1.src.rpm
+
+ls  /github/home/rpmbuild/RPMS/x86_64
+
+
 if [ -z "$GITHUB_WORKSPACE" ]; then
     echo "GITHUB_WORKSPACE is not set"
     exit 1
@@ -7,10 +32,10 @@ fi
 
 DEST_DIR=$GITHUB_WORKSPACE/rpms
 
-echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
+echo "Copying prebuilt packages for $1 to $DEST_DIR"
 mkdir -p "$DEST_DIR"
-cp /root/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
+cp /github/home/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
 
-echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
+echo "::set-output name=built-version::$1"
 
 exit 0
