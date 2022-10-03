ff34c84bbf8bc4284e4d89cf5085b5e647b8ec4a
slohe1 <saurabh.lohe@globallogic.com>
2022-10-03 20:09:30 +0530

    OPSEXP-1739 tesing for ubuntu18

--- .github/actions/Ubuntu18.04-build/entrypoint.sh
+++ .github/actions/Ubuntu18.04-build/entrypoint.sh
@@ -9,19 +9,17 @@ cd ImageMagick && \
     apt-get install -y dpkg-dev devscripts gawk ghostscript gsfonts libzstd-dev libbz2-dev libdjvulibre-dev libfftw3-dev fontconfig libfreetype6-dev libgs-dev libjbig-dev libjpeg8-dev libjpeg-turbo8-dev liblcms2-dev liblqr-1-0-dev libltdl-dev liblzma-dev libopenexr-dev libopenjp2-7-dev libpango1.0-dev libperl-dev libpng-dev libraqm-dev libraw-dev librsvg2-dev libtiff5-dev libwebp-dev libwmf-dev libx11-dev libxext-dev libxml2-dev libxt-dev libzip-dev zlib1g-dev && \
     yes | mk-build-deps -Bi && dpkg-buildpackage -b -uc
 
-find /-name imagemagick_7.1.0-16_amd64.deb
+if [ -z "$GITHUB_WORKSPACE" ]; then
+    echo "GITHUB_WORKSPACE is not set"
+    exit 1
+fi
 
-#if [ -z "$GITHUB_WORKSPACE" ]; then
-#    echo "GITHUB_WORKSPACE is not set"
-#    exit 1
-#fi
+DEST_DIR=$GITHUB_WORKSPACE/packages
 
-#DEST_DIR=$GITHUB_WORKSPACE/packages
+echo "Copying prebuilt packages for $1 to $DEST_DIR"
+mkdir -p "$DEST_DIR"
+cp /github/workspace/*.deb "$DEST_DIR" 
 
-#echo "Copying prebuilt packages for $1 to $DEST_DIR"
-#mkdir -p "$DEST_DIR"
-#cp /github/workspace/*.deb "$DEST_DIR" 
+echo "::set-output name=built-version::$1"
 
-#echo "::set-output name=built-version::$1"
-
-#exit 0
+exit 0
