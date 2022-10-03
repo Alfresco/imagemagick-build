34cdd0ce480d95c65ce0ba9df5ae71920cbf8e89
slohe1 <saurabh.lohe@globallogic.com>
2022-10-03 15:05:56 +0530

    OPSEXP-1739 Testing for dockerfile changes centos7

--- .github/actions/centos-build/entrypoint.sh
+++ .github/actions/centos-build/entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/bash -e
-echo "! $1"
+##Build dependencies
+yum install -y epel-release git make && \
+    yum-config-manager --enable powertools && \
+    yum install -y rpm-build yum-utils && \
+    yum group install -y "Development Tools" && \
+    yum clean all
+
+## Imagemagick sources
+git clone --depth 1 -b $1 https://github.com/ImageMagick/ImageMagick.git
+
+## Generate updated .src.rpm
+cd ImageMagick && \
+    ./configure && \
+    make dist-xz && \
+    sed -i '/BuildRequires.*lqr/d' ImageMagick.spec && \
+    make srpm
+
+## Finally build rpm
+cd ImageMagick && \
+    yum-builddep -y ImageMagick-$1.src.rpm && \
+    rpmbuild --rebuild --nocheck ImageMagick-$1.src.rpm
+
+ls /root/rpmbuild/RPMS/x86_64
+
 if [ -z "$GITHUB_WORKSPACE" ]; then
     echo "GITHUB_WORKSPACE is not set"
     exit 1
@@ -7,10 +30,10 @@ fi
 
 DEST_DIR=$GITHUB_WORKSPACE/rpms
 
-echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
+echo "Copying prebuilt packages for $1 to $DEST_DIR"
 mkdir -p "$DEST_DIR"
 cp /root/rpmbuild/RPMS/x86_64/*.rpm  "$DEST_DIR"
 
-echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
+echo "::set-output name=built-version::$1"
 
 exit 0
