c9b587f8e8125f90533420cb76b6622800b5b1f8
slohe1 <105858985+slohe1@users.noreply.github.com>
2022-10-04 21:39:42 +0530

    OPSEXP-1739 Single place for Imagemagick version (#8)

--- .github/actions/Ubuntu20.04-build/entrypoint.sh
+++ .github/actions/Ubuntu20.04-build/entrypoint.sh
@@ -5,12 +5,15 @@ if [ -z "$GITHUB_WORKSPACE" ]; then
     exit 1
 fi
 
-DEST_DIR=$GITHUB_WORKSPACE/packages
+apt-get update && apt-get upgrade -y &&  apt-get install build-essential git automake equivs -y
 
-echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
-mkdir -p "$DEST_DIR"
-cp /root/*.deb "$DEST_DIR" 
+git clone --depth 1 -b $1 https://github.com/ImageMagick/ImageMagick.git
 
-echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
+cd ImageMagick && \
+    mv /tmp/debian . && \
+    apt-get install -y dpkg-dev devscripts gawk ghostscript gsfonts libzstd-dev libbz2-dev libdjvulibre-dev libfftw3-dev fontconfig libfreetype6-dev libgs-dev libjbig-dev libjpeg8-dev libjpeg-turbo8-dev liblcms2-dev liblqr-1-0-dev libltdl-dev liblzma-dev libopenexr-dev libopenjp2-7-dev libpango1.0-dev libperl-dev libpng-dev libraqm-dev libraw-dev librsvg2-dev libtiff5-dev libwebp-dev libwmf-dev libx11-dev libxext-dev libxml2-dev libxt-dev libzip-dev zlib1g-dev && \
+    yes | mk-build-deps -Bi && dpkg-buildpackage -b -uc
+
+echo "::set-output name=built-version::$1"
 
 exit 0
--- .github/actions/rockylinux-build/entrypoint.sh
+++ .github/actions/rockylinux-build/entrypoint.sh
@@ -5,12 +5,36 @@ if [ -z "$GITHUB_WORKSPACE" ]; then
     exit 1
 fi
 
+# Build dependencies
+dnf install -y epel-release git make wget 'dnf-command(config-manager)' && \
+    dnf config-manager --set-enabled powertools && \
+    dnf install -y rpm-build yum-utils && \
+    dnf group install -y "Development Tools" && \
+    dnf clean all
+
+# Imagemagick sources
+git clone --depth 1 -b $1 https://github.com/ImageMagick/ImageMagick.git
+
+# Generate updated .src.rpm
+cd ImageMagick && \
+    ./configure && \
+    make dist-xz && \
+    sed -i '/BuildRequires.*lqr/d' ImageMagick.spec && \
+    make srpm
+
+# Finally build rpm
+ cd $GITHUB_WORKSPACE/ImageMagick && \
+    yum-builddep -y ImageMagick-$1.src.rpm && \
+    rpmbuild --rebuild --nocheck ImageMagick-$1.src.rpm
+
+ls  /github/home/rpmbuild/RPMS/x86_64
+
 DEST_DIR=$GITHUB_WORKSPACE/rpms
 
-echo "Copying prebuilt packages for $IMAGEMAGICK_VERSION to $DEST_DIR"
+echo "Copying prebuilt packages for $1 to $DEST_DIR"
 mkdir -p "$DEST_DIR"
-cp /root/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
+cp /github/home/rpmbuild/RPMS/x86_64/*.rpm "$DEST_DIR"
 
-echo "::set-output name=built-version::$IMAGEMAGICK_VERSION"
+echo "::set-output name=built-version::$1"
 
 exit 0
