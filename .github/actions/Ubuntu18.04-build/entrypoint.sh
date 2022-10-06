1ed566e3d758148f202d216776b9c8a5c1354035
slohe1 <saurabh.lohe@globallogic.com>
2022-10-06 14:47:28 +0530

    OPSEXP-1745 ubuntu test for 7.0

--- .github/actions/Ubuntu18.04-build/entrypoint.sh
+++ .github/actions/Ubuntu18.04-build/entrypoint.sh
@@ -11,6 +11,7 @@ git clone --depth 1 -b $1 https://github.com/ImageMagick/ImageMagick.git
 
 cd ImageMagick && \
     mv /tmp/debian . && \
+    sed -i "1s/7.1.0-16/$1/g" "debian/changelog" "debian/README.Debian" && \
     apt-get install -y dpkg-dev devscripts gawk ghostscript gsfonts libzstd-dev libbz2-dev libdjvulibre-dev libfftw3-dev fontconfig libfreetype6-dev libgs-dev libjbig-dev libjpeg8-dev libjpeg-turbo8-dev liblcms2-dev liblqr-1-0-dev libltdl-dev liblzma-dev libopenexr-dev libopenjp2-7-dev libpango1.0-dev libperl-dev libpng-dev libraqm-dev libraw-dev librsvg2-dev libtiff5-dev libwebp-dev libwmf-dev libx11-dev libxext-dev libxml2-dev libxt-dev libzip-dev zlib1g-dev && \
     yes | mk-build-deps -Bi && dpkg-buildpackage -b -uc
 
