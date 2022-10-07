e6519ae39f593f7f410605ecd06e08f329f81e54
slohe1 <105858985+slohe1@users.noreply.github.com>
2022-10-07 20:16:29 +0530

    OPSEXP-1745 Imagemagick-7.0.10 Build (#9)

--- .github/actions/Ubuntu18.04-build/debian/preinst.ex
+++ .github/actions/Ubuntu18.04-build/debian/preinst.ex
@@ -1,5 +1,5 @@
 #!/bin/sh
-# preinst script for imagemagick-7.1.0-16
+# preinst script for imagemagick-7
 #
 # see: dh_installdeb(1)
 
--- .github/actions/Ubuntu20.04-build/debian/prerm.ex
+++ .github/actions/Ubuntu20.04-build/debian/prerm.ex
@@ -1,5 +1,5 @@
 #!/bin/sh
-# prerm script for imagemagick-7.1.0-16
+# prerm script for imagemagick-7
 #
 # see: dh_installdeb(1)
 
