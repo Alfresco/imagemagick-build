3bc3db2717a85190d60dcca27d6ec68bda111ebf
slohe1 <saurabh.lohe@globallogic.com>
2023-08-22 14:52:20 +0530

    OPSEXP-1843 build and test

--- rpms/run_test.sh
+++ rpms/run_test.sh
@@ -4,7 +4,6 @@ IMAGEMAGICK_VERSION=$1
 TARGET_ARCH=$2
 
 # Install the RPMs
-IMAGEMAGICK_VERSION=$1
 yum install -y /root/rpmbuild/RPMS/${TARGET_ARCH}/ImageMagick-libs-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
 yum install -y /root/rpmbuild/RPMS/${TARGET_ARCH}/ImageMagick-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
 
