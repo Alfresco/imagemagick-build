3799967d33aef2deb54da6b6b750dfdb81b74dbb
slohe1 <saurabh.lohe@globallogic.com>
2023-10-11 23:56:58 +0530

    OPSEXP-2333 refactor ubuntu test

---
+++ test_debs/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e -o pipefail -x
+
+if [ -z "$IMAGE_TAG" ]; then
+    echo "IMAGE_TAG env needs to be set"
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "first argument needs to be set - IMAGEMAGICK_VERSION"
+    exit 1
+fi
+
+IMAGEMAGICK_VERSION=$1
+
+echo "Testing package expected dependencies"
+dpkg-deb -f imagemagick-alfresco_${IMAGEMAGICK_VERSION}_amd64.deb  Depends | grep -qEv 'libcdt|libcgraph|libgvc|libgs9'
+
+echo "Installing package"
+dpkg -i imagemagick-alfresco_${IMAGEMAGICK_VERSION}_amd64.deb
+
+echo "Exporting the path of the package"
+export PATH="/opt/imagemagick-7/bin:$PATH"
+
+echo "Testing convert command"
+convert -version
+
+echo "Creating test image file"
+convert  -size 32x32 xc:transparent test.png
+
+echo "Converting png to jpg"
+convert test.png test1.jpg
+
+exit 0
