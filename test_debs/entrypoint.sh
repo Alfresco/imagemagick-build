dccbb721041039ab52b10867085b35b989408675
slohe1 <saurabh.lohe@globallogic.com>
2023-10-12 16:20:04 +0530

    OPSEXP-2333 change installation cmd for ubuntu

--- test_debs/entrypoint.sh
+++ test_debs/entrypoint.sh
@@ -13,14 +13,12 @@ fi
 
 IMAGEMAGICK_VERSION=$1
 
-echo "Install dependencies"
-apt-get install liblcms2-utils libfribidi0 libilmbase12 libdjvulibre21 librsvg2-2 libwmf0.2-7 libgomp1 liblqr-1-0 libltdl7 libopenexr22 libopenjp2-7 libraqm0 libraw16 libwebp6 libwebpdemux2 libwebpmux3 libzip4
-
 echo "Testing package expected dependencies"
 dpkg-deb -f imagemagick-alfresco_${IMAGEMAGICK_VERSION}_amd64.deb  Depends | grep -qEv 'libcdt|libcgraph|libgvc|libgs9'
 
 echo "Installing package"
-dpkg -i imagemagick-alfresco_${IMAGEMAGICK_VERSION}_amd64.deb
+dpkg -i --force-depends imagemagick-alfresco_${IMAGEMAGICK_VERSION}_amd64.deb
+apt-get -f install
 
 echo "Exporting the path of the package"
 export PATH="/opt/imagemagick-7/bin:$PATH"
