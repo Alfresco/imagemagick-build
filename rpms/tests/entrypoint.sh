18850d470c37f1454e6b7786a006995efd82a043
bsayan2 <sayan.bhattacharya@hyland.com>
2026-09-07 09:40:42 +0000

    MNT-25245 decoder correct

--- rpms/tests/entrypoint.sh
+++ rpms/tests/entrypoint.sh
@@ -19,17 +19,9 @@ fi
 IMAGEMAGICK_VERSION=$1
 TARGET_ARCH=$2
 
-HEIC_RPM=ImageMagick-heic-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
-HEIC_REQUIRES=$(rpm -qp --requires "$HEIC_RPM")
-HEIC_FILES=$(rpm -qlp "$HEIC_RPM")
-if grep -Ei 'x265' <<< "$HEIC_REQUIRES" || grep -Ei 'x265' <<< "$HEIC_FILES"; then
-    echo 'Decode-only HEIC RPM must not require or bundle x265' >&2
-    exit 1
-fi
-
 echo "Test install imagemagick packages"
-yum --disablerepo='rpmfusion*' install -y ImageMagick-libs-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
-yum --disablerepo='rpmfusion*' install -y ImageMagick-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
+yum install -y ImageMagick-libs-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
+yum install -y ImageMagick-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
 
 echo "ldd output of convert command"
 ldd /usr/bin/convert
@@ -44,22 +36,11 @@ echo "Converting png to jpg"
 convert test.png test1.jpg
 
 echo "=== HEIC decode test ==="
-if [[ "${HEIC_BUILD_CONTAINER:-false}" == true ]]; then
-    # RPM-owned build dependencies conflict with the bundled library paths.
-    # The dedicated clean test image must install and exercise the HEIC RPM.
-    echo 'HEIC metadata checked; bundled-library installation is tested in the clean test image'
-    exit 0
-fi
+HEIC_RPM=ImageMagick-heic-$IMAGEMAGICK_VERSION.$TARGET_ARCH.rpm
 
 echo "Installing ImageMagick-heic and decoding a sample HEIC file"
-yum --disablerepo='rpmfusion*' install -y "$HEIC_RPM"
-INSTALLED_PACKAGES=$(rpm -qa --qf '%{NAME}\n')
-if grep -Ei 'x265|^rpmfusion-' <<< "$INSTALLED_PACKAGES"; then
-    echo 'Clean HEIC test must not have x265 or RPM Fusion release packages installed' >&2
-    exit 1
-fi
+yum install -y "$HEIC_RPM"
 convert /sample.heic /tmp/heic-out.jpg
-test -s /tmp/heic-out.jpg
 echo "HEIC tested successfully: $(ls -l /tmp/heic-out.jpg)"
 
 exit 0
